912762403449ccc4ca32ce1fe931e6331714230c
RenzoJimenezB <adrianrjb@outlook.es>
2024-10-25 20:11:20 -0500

    generated resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { OrderDetailsModule } from './modules/order-details/order-details.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    OrderDetailsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
