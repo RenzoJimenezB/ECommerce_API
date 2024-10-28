2e1b4db35d0f22a76033f0fde36f6c654a75e5e0
RenzoJimenezB <adrianrjb@outlook.es>
2024-10-27 19:43:27 -0500

    entities & connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,21 @@ import { ProductsModule } from './modules/products/products.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { OrderDetailsModule } from './modules/order-details/order-details.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
     UsersModule,
     ProductsModule,
     CategoriesModule,
---
+++ src/config/typeorm.ts
@@ -0,0 +1,24 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({
+  path: '.env.development',
+});
+
+const config = {
+  type: 'postgres',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*.{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: true,
+};
+
+export default registerAs('typeorm', () => config);
+
+export const connectionSource = new DataSource(config as DataSourceOptions);
