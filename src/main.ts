eedd32e097c156d85ddad27d02c8937dcb981af9
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-23 23:43:26 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('E-Commerce API')
-    .setDescription('E-Commerce Demo API')
-    .setVersion('1.0')
+    .setTitle("E-Commerce API")
+    .setDescription("E-Commerce Demo API")
+    .setVersion("1.0")
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
+      forbidNonWhitelisted: true
+    })
   );
 
-  await app.listen(3000);
+  await app.listen(3000, "0.0.0.0");
 }
+
 bootstrap();
