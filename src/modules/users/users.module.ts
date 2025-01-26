e0be27d93fd879f7c942aca21fc82e675eb5b506
RenzoJimenezB <adrianrjb@outlook.es>
2025-01-26 16:26:58 -0500

    update

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,31 +1,32 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-import { UsersRepository } from './users.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { UsersRepository } from "./users.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
 
 @Module({
   controllers: [UsersController],
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService, UsersRepository],
-  exports: [UsersRepository],
+  exports: [UsersRepository]
 })
 export class UsersModule implements OnModuleInit {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {
+  }
 
   async onModuleInit(): Promise<void> {
     const superAdminExists = await this.usersService.checkIfSuperAdminExists();
 
     if (superAdminExists) {
-      console.log('Superadmin already exists, skipping creation');
+      console.log("SuperAdmin already exists, skipping creation");
       return;
     }
 
     await this.usersService.createSuperAdmin({
-      name: 'superadmin',
+      name: "superAdmin",
       email: process.env.SUPERADMIN_EMAIL,
-      password: process.env.SUPERADMIN_PASSWORD,
+      password: process.env.SUPERADMIN_PASSWORD
     });
   }
 }
